0b7d0e241c85d2acdbb4f2b774a61fcdcad722f8
thegeronimo <thegeronimo@protonmail.com>
2019-12-28 07:21:07 -0500

    add out of the money + put test cases

--- test/blackscholes_spec.js
+++ test/blackscholes_spec.js
@@ -57,5 +57,75 @@ contract("BlackScholes", function() {
       const contractBS = await BlackScholes.methods.retBlackScholesCalc(price, strike, oneYear.unix(), vol, rfr, call).call();
       assert.strictEqual(lessThanCent(localBS, contractBS), true, "difference more than one cent")
     })
+
+    it('correctly prices out of the money call with one year time', async () => {
+      const strike = toEth('350');
+      const price = toEth('300');
+      const now = moment();
+      const oneYear = moment(now).add(12, 'M');
+      const diff = oneYear.from(now)
+      const time = genOptionTime(now, oneYear);
+      const vol = 15;
+      const rfr = 3;
+      const localBS = bs.blackScholes(300, 350, time, .15, .03, "call");
+      const contractBS = await BlackScholes.methods.retBlackScholesCalc(price, strike, oneYear.unix(), vol, rfr, call).call();
+      assert.strictEqual(lessThanCent(localBS, contractBS), true, "difference more than one cent")
+    })
+
+    it('correctly prices at the money call with one year time', async () => {
+      const strike = toEth('300');
+      const price = toEth('300');
+      const now = moment();
+      const oneYear = moment(now).add(12, 'M');
+      const diff = oneYear.from(now)
+      const time = genOptionTime(now, oneYear);
+      const vol = 15;
+      const rfr = 3;
+      const localBS = bs.blackScholes(300, 300, time, .15, .03, "call");
+      const contractBS = await BlackScholes.methods.retBlackScholesCalc(price, strike, oneYear.unix(), vol, rfr, call).call();
+      assert.strictEqual(lessThanCent(localBS, contractBS), true, "difference more than one cent")
+    })
+
+    it('correctly prices near the money call with one year time', async () => {
+      const strike = toEth('310');
+      const price = toEth('300');
+      const now = moment();
+      const oneYear = moment(now).add(12, 'M');
+      const diff = oneYear.from(now)
+      const time = genOptionTime(now, oneYear);
+      const vol = 15;
+      const rfr = 3;
+      const localBS = bs.blackScholes(300, 310, time, .15, .03, "call");
+      const contractBS = await BlackScholes.methods.retBlackScholesCalc(price, strike, oneYear.unix(), vol, rfr, call).call();
+      assert.strictEqual(lessThanCent(localBS, contractBS), true, "difference more than one cent")
+    })
+
+    it('correctly prices out of the money call with one year time high volatility', async () => {
+      const strike = toEth('350');
+      const price = toEth('300');
+      const now = moment();
+      const oneYear = moment(now).add(12, 'M');
+      const diff = oneYear.from(now)
+      const time = genOptionTime(now, oneYear);
+      const vol = 150;
+      const rfr = 3;
+      const localBS = bs.blackScholes(300, 350, time, 1.5, .03, "call");
+      const contractBS = await BlackScholes.methods.retBlackScholesCalc(price, strike, oneYear.unix(), vol, rfr, call).call();
+      assert.strictEqual(lessThanCent(localBS, contractBS), true, "difference more than one cent")
+    })
+
+    it('correctly prices in the money put with one year time', async () => {
+      const strike = toEth('250');
+      const price = toEth('200');
+      const now = moment();
+      const oneYear = moment(now).add(12, 'M');
+      const diff = oneYear.from(now)
+      const time = genOptionTime(now, oneYear);
+      const vol = 15;
+      const rfr = 3;
+      const localBS = bs.blackScholes(200, 250, time, .15, .03, "put");
+      const contractBS = await BlackScholes.methods.retBlackScholesCalc(price, strike, oneYear.unix(), vol, rfr, put).call();
+      assert.strictEqual(lessThanCent(localBS, contractBS), true, "difference more than one cent")
+    })
   })
 })
